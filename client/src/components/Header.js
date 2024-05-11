750f0ed1628f9d4aa1e5be2ae3361a52edb8193f
DoanCasotti <doancasotti@outlook.com>
2024-05-10 21:35:14 -0300

    fazendo alteracoes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2024",
+  title: "BIA 2024 Respondendo via pipeline + BEANSTALK",
 };
 
 export default Header;
